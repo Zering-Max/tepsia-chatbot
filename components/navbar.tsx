9bf6a57208c60bce81f54a3499b71323beea7205
Maxime Lévêque <maxlev.oclock@gmail.com>
2026-09-19 10:06:32 +0200

    clean code

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Button } from "./ui/button";
-// import { GitIcon } from "./icons";
 import Link from "next/link";
 
 export const Navbar = () => {
@@ -9,12 +7,11 @@ export const Navbar = () => {
   return (
     <div className="p-2 flex flex-row gap-2 justify-center items-center">
       <span className="text-xs">
-        Cet assistant conversationnel a été codé en
+        Cet assistant conversationnel a été développé en
       </span>
       <Link href={`${url}`} target="_blank" className="text-xs underline underline-offset-4">
         open-source
       </Link>
-      
     </div>
   );
 };
